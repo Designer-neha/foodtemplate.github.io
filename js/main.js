0c31a7b643493bebf9f58da49ecb2024fef137b4
Designer-neha <webneha089@gmail.com>
2019-09-22 15:21:22 +0530

    publishing
    foodorder template design

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed('.typing', {
+  strings: ["Let's the best places to eat, drink, and shop nearest to you"],
+  loop:true,
+  typeSpeed: 40,
+  backSpeed:40,
+  backDelay:1000,
+});
+
+
